b1fa891ddcf93db218f86eeab4224a58cf434952
Falguni Yadav ( Kumkum ) <falguniyadav1430@gmail.com>
2025-05-13 22:40:49 +0530

    Create List.jsx

---
+++ Admin/src/pages/List/List.jsx
@@ -0,0 +1,72 @@
+import React, { useEffect, useState } from 'react';
+import './List.css';
+import axios from "axios";
+import { toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+const List = () => {
+  const url = "http://localhost:4000";
+  const [list, setList] = useState([]);
+
+  const fetchList = async () => {
+    try {
+      const response = await axios.get(`${url}/api/food/list`);
+
+      if (response.data.success) {
+        setList(response.data.data);
+      } else {
+        toast.error("Error fetching food list");
+      }
+    } catch (error) {
+      toast.error("Failed to fetch data");
+      console.error("Fetch error:", error);
+    }
+  };
+
+  const removeFood = async (foodId) => {
+    const response = await axios.post(`${url}/api/food/remove`, { id: foodId })
+    await fetchList();
+    if (response.data.success) {
+      toast.success(response.data.message)
+    }
+    else {
+      toast.error("Error")
+    }
+  }
+
+  useEffect(() => {
+    fetchList();
+  }, []);
+
+  return (
+    <div className='list add flex-col'>
+      <p>All Food Lists</p>
+      <div className="list-table">
+        <div className="list-table-format title">
+          <b>Image</b>
+          <b>Name</b>
+          <b>Category</b>
+          <b>Price</b>
+          <b>Action</b>
+        </div>
+        {list.length > 0 ? (
+          list.map((item, index) => (
+            <div key={index} className='list-table-format'>
+              <img src={`${url}/images/${item.image}`} alt={item.name} />
+              <p>{item.name}</p>
+              <p>{item.category}</p>
+              <p>${item.price}</p>
+              <p onClick={() => removeFood(item._id)} className='cursor'>X</p>
+            </div>
+          ))
+        ) : (
+          <p>No food items available</p>
+        )}
+      </div>
+
+      <ToastContainer />
+    </div>
+  );
+};
+
+export default List;
